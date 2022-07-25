7fe287ca16d33ba77f7697c1272cad374ff5f046
edward-burn <9583964+edward-burn@users.noreply.github.com>
2022-07-25 17:38:27 +0200

    roxygen added

--- R/matches.R
+++ R/matches.R
@@ -1,3 +1,14 @@
+#' Create a standard dataframe for league data
+#'
+#' @param data_engsoc Data produced by engsoccerdata
+#' @param country_name Name of country that will be added to output to
+#' identify country used
+#'
+#' @return a tibble with columns `country`, `date`, `season`, `tier`, `home`,
+#' `visitor`, `goals_home`, `goals_visitor`.
+#' @export
+#'
+#' @examples
 uss_make_matches <- function(data_engsoc, country_name) {
   result <-
     data_engsoc |>
