aa98b73e24b5aef6fb32a6b43d93b67cadf158d9
edward-burn <9583964+edward-burn@users.noreply.github.com>
2022-07-26 19:37:10 +0200

    various updates

--- R/matches.R
+++ R/matches.R
@@ -1,28 +1,38 @@
-#' Create a standard dataframe for league data
+#' Make a standard league-play tibble
 #'
-#' @param data_engsoc Data produced by engsoccerdata
-#' @param country_name Name of country that will be added to output to
-#' identify country used
+#' Given a league-play data frame from {engsoccer}, returns a tibble with
+#' standardised colomn-names and types, e.g. `date` is a `Date`.
 #'
-#' @return a tibble with columns `country`, `date`, `season`, `tier`, `home`,
-#' `visitor`, `goals_home`, `goals_visitor`.
-#' @export
+#' @param data_engsoc `data.frame` obtained from {engsoccerdata}.
+#' @param country `character` scalar, specifies the league.
+#'   `uss_countries()` returns choices available from {engsoccerdata}
+#'
+#' @return [tibble][tibble::tibble-package] with columns `country`, `date`,
+#'  `season`, `tier`, `home`, `visitor`, `goals_home`, `goals_visitor`.
 #'
 #' @examples
-#' uss_make_matches(engsoccerdata::england, country_name = "England")
-uss_make_matches <- function(data_engsoc, country_name) {
+#' uss_make_matches(engsoccerdata::italy, "Italy")
+#' @keywords internal
+#' @export
+#'
+uss_make_matches <- function(data_engsoc, country) {
+  
+  # validate
+
+  # put into "standard" form
   result <-
     data_engsoc |>
     tibble::as_tibble() |>
     dplyr::transmute(
-      country=country_name,
-      tier = factor(tier, levels = c("1", "2", "3", "4")),
-      season = as.integer(Season),
-      date = as.Date(Date),
-      home = as.character(home),
-      visitor = as.character(visitor),
-      goals_home = as.integer(hgoal),
-      goals_visitor = as.integer(vgoal)
+      country = as.character(.env$country),
+      tier = factor(.data$tier, levels = c("1", "2", "3", "4")),
+      season = as.integer(.data$Season),
+      date = as.Date(.data$Date),
+      home = as.character(.data$home),
+      visitor = as.character(.data$visitor),
+      goals_home = as.integer(.data$hgoal),
+      goals_visitor = as.integer(.data$vgoal)
     )
+  
   result
 }
--- tests/testthat/test-matches.R
+++ tests/testthat/test-matches.R
@@ -1,11 +1,10 @@
 test_that("uss_make_matches works", {
-
-  # use the function
+  
   italy <- uss_make_matches(engsoccerdata::italy, "Italy")
+  
   expect_true(tibble::is_tibble(italy))
-  expect_named(italy,
-    c("country", "tier", "season", "date", "home", "visitor",
-      "goals_home", "goals_visitor")
-  )
-    expect_identical(unique(italy$country), "Italy")
+  expect_named(italy, cols_matches())
+  expect_identical(unique(italy$country), "Italy")
+
+  expect_s3_class(italy$tier, "factor")
 })
